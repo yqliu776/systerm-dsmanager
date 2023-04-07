194d991325eb164589a8e23d99813ee834e64d48
kevin <yqliumail@163.com>
2023-04-07 22:45:18 +0800

    完善 商品数据模型

--- models.py
+++ models.py
@@ -56,10 +56,23 @@ class UserModel(db.Model):
 
 
 class ProductsModel(db.Model):
-    """
-    商品 数据表 模型
-    属性：id， name······
-    """
     __tablename__ = 'products'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    product_id = db.Column(db.String(100), unique=True)
     product_name = db.Column(db.String(200))
+    product_image_url = db.Column(db.String(500))
+    original_price = db.Column(db.Float)
+    discount_price = db.Column(db.Float)
+    product_url = db.Column(db.String(500))
+    category = db.Column(db.String(100))
+    subcategory = db.Column(db.String(200))
+
+    def __init__(self, product_id, product_name, product_image_url, original_price, discount_price, product_url, category, subcategory):
+        self.product_id = product_id
+        self.product_name = product_name
+        self.product_image_url = product_image_url
+        self.original_price = original_price
+        self.discount_price = discount_price
+        self.product_url = product_url
+        self.category = category
+        self.subcategory = subcategory
