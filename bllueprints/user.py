bdd7974727c8ef5723b6a40a4285f65f04386364
kevin <yqliumail@163.com>
2023-04-13 06:58:16 +0800

    用户登录

--- bllueprints/user.py
+++ bllueprints/user.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template, session, redirect, url_for
+from flask import Blueprint, render_template, session, redirect, url_for, request
+from werkzeug.security import check_password_hash
+
+from forms import UserLoginForm
+from models import UserModel
 
 bp = Blueprint('user', __name__, url_prefix='/user')
 
@@ -12,19 +16,44 @@ def index():
     return render_template('user/index_user.html', is_logged_in=is_logged_in)
 
 
-@bp.route('/login')
+@bp.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('user/login_user.html')
+    """
+    User login。
+    :return:
+    """
+    if request.method == 'GET':
+        return render_template('user/login_user.html')
+    else:
+        form = UserLoginForm(request.form)
+        if form.validate():
+            username = form.username.data
+            password = form.password.data
+            user = UserModel.query.filter_by(username=username).first()
+            if user is None:
+                error = '用户不存在！'
+                return render_template('user/login_user.html', error=error)
+            else:
+                # print(generate_password_hash(password))
+                if user and check_password_hash(user.password, password):
+                    session['admin_id'] = user.id
+                    session['name'] = user.username
+                    return redirect(url_for('user.index'))
+                else:
+                    error = '账户密码错误！'
+                    return render_template('user/login_user.html', error=error)
+        else:
+            error = '帐号或密码格式不正确，请重新输入。'
+            return render_template('user/login_user.html', error=error)
 
 
 @bp.route('/logout')
 def logout():
     session.pop('user_id', None)
-    session.pop('permission', None)
     session.pop('name', None)
     return redirect(url_for('user.index'))
 
 
-@bp.route('/register')
+@bp.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('user/register.html')
+    return render_template('user/register_user.html')
--- forms.py
+++ forms.py
@@ -13,8 +13,24 @@ class AdminLoginForm(wtforms.Form):
 
 class AdminAddForm(wtforms.Form):
     """
-    admin add 表单
+    admin 添加 表单
     """
     adminname = wtforms.StringField(validators=[length(min=3, max=25)])
     password = wtforms.StringField(validators=[length(min=6, max=16)])
     Permission = wtforms.StringField(validators=[length(min=1, max=100)])
+
+
+class UserLoginForm(wtforms.Form):
+    """
+    user 登陆 表单
+    """
+    username = wtforms.StringField(validators=[length(min=3, max=25)])
+    password = wtforms.StringField(validators=[length(min=6, max=16)])
+
+
+class UserRegisterForm(wtforms.Form):
+    """
+    user 注册 表单
+    """
+    username = wtforms.StringField(validators=[length(min=3, max=25)])
+    password = wtforms.StringField(validators=[length(min=6, max=16)])
